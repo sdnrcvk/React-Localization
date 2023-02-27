6258e9a05fb287baf4a5e92a6f529a54399e285b
Sedanur Çevik <sdnrcvk@gmail.com>
2023-02-27 15:10:42 +0300

    defaultLocale described

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
-import {IntlProvider, FormattedMessage, FormattedNumber} from 'react-intl'
-import { useState } from 'react';
+import {IntlProvider, FormattedMessage} from 'react-intl'
+import { useEffect, useState } from 'react';
 
 const messages = {
     "tr-TR":{
@@ -15,20 +15,30 @@ const messages = {
 
 
 function App() {
-const [lang,setLang]=useState("tr-TR");
+  const isLocale=localStorage.getItem("locale");
+  const defaultLocale=isLocale ? isLocale : navigator.language;
+  const [locale,setLocale]=useState(defaultLocale);
+
+
+  useEffect(()=>{
+    localStorage.setItem("locale",locale);
+
+  },[locale]);
+
 
   return (
     <div className="App">
         <IntlProvider 
-          messages={messages[lang]}
+          locale={locale}
+          messages={messages[locale]}
         >
           <FormattedMessage id="title"/>
           <p>
             <FormattedMessage id="description"/>
           </p>
           <br />
-          <button onClick={()=>setLang("tr-TR")}>TR</button>
-          <button onClick={()=>setLang("en-US")}>EN</button>
+          <button onClick={()=>setLocale("tr-TR")}>TR</button>
+          <button onClick={()=>setLocale("en-US")}>EN</button>
         </IntlProvider>
     </div>
   );
