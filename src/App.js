803ccf9352988c8ec89baa52c0cecc42bbac0a6a
Sedanur Çevik <sdnrcvk@gmail.com>
2023-02-27 14:59:30 +0300

    IntProvider, FormattedMessage used

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
 import './App.css';
-import {IntlProvider, FormattedRelative, useIntl} from 'react-intl';
+import {IntlProvider, FormattedMessage, FormattedNumber} from 'react-intl'
+import { useState } from 'react';
+
+const messages = {
+    "tr-TR":{
+      title: "Merhaba Kullanıcı",
+      description:"3 yeni mesajınız var"
+    },
+    "en-US":{
+      title: "Hello User",
+      description:"You have 3 new messages",
+    }
+};
+
 
 function App() {
+const [lang,setLang]=useState("tr-TR");
+
   return (
     <div className="App">
-      
+        <IntlProvider 
+          messages={messages[lang]}
+        >
+          <FormattedMessage id="title"/>
+          <p>
+            <FormattedMessage id="description"/>
+          </p>
+          <br />
+          <button onClick={()=>setLang("tr-TR")}>TR</button>
+          <button onClick={()=>setLang("en-US")}>EN</button>
+        </IntlProvider>
     </div>
   );
 }
