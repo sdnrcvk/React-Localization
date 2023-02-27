b5987befd1e9b38c7ad6a230abef5e247fa4fd4c
Sedanur Çevik <sdnrcvk@gmail.com>
2023-02-27 14:38:51 +0300

    libraries imported

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import {IntlProvider, FormattedRelative, useIntl} from 'react-intl';
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
